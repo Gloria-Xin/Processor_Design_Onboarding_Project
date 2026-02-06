82d6d1f3c8f9abe005dbb82bfdbe6fc0b0bf99a2
tonykorycki <koryckiantoni@gmail.com>
2026-02-06 16:53:38 -0500

    deleted unused template directories

--- dv/Module.cpp
+++
@@ -1,9 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-#include <NyuCatch2TestUtil.hpp>
-
-#include <VModule.h>
-
-TEST_CASE("Test") {
-  auto& mod {nyu::get_dut_catch2<VModule>()};
-  REQUIRE(1);
-}
